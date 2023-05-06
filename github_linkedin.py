f76f14f492e477a1e3d12e8e7129052adac29cc6
Thabish <kadertabish2@gmail.com>
2023-05-06 14:01:25 +0400

    twitter.py
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- github_linkedin.py
+++ github_linkedin.py
@@ -3,32 +3,42 @@ from github import Github
 from dotenv import load_dotenv
 from linkedin_api import Linkedin
 import tweepy
+import requests
 
 load_dotenv()
 acess_token = os.environ.get('ACESS_TOKEN')
 consumer_key = os.environ.get('TWITTER_API_KEY')
-consumer_secret = os.environ.get('TWITTER_KEY_SECRET')
+consumer_secret = os.environ.get('TWITTER_API_SECRET')
 twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
 twitter_access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-
-#  ----- Github ------ 
-g = Github(acess_token)
-latest_repo = None
-# Loop over repos and find the latest repo
-for repo in g.get_user().get_repos():
-    if not latest_repo or repo.updated_at > latest_repo.updated_at:
-        latest_repo = repo
-contents = latest_repo.get_contents("readme.md")
-readme_content = contents.decoded_content.decode("utf-8")
+twitter_bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
+# #  ----- Github ------ 
+# g = Github(acess_token)
+# latest_repo = None
+# # Loop over repos and find the latest repo
+# for repo in g.get_user().get_repos():
+#     if not latest_repo or repo.updated_at > latest_repo.updated_at:
+#         latest_repo = repo
+# contents = latest_repo.get_contents("readme.md")
+# readme_content = contents.decoded_content.decode("utf-8")
 
 
 # ----- Twitter ------
-auth = tweepy.OAuth1UserHandler(
-    consumer_key, consumer_secret,
-    twitter_access_token, twitter_access_token_secret
+
+client = tweepy.Client(
+   bearer_token=twitter_bearer_token,
+   consumer_key=consumer_key,
+   consumer_secret=consumer_secret,
+   access_token=twitter_access_token,
+   access_token_secret=twitter_access_token_secret
 )
-api = tweepy.API(auth)
-print(api.get_friends())
+print(client.search_all_tweets(query="Cars"))
+
+
+
+
+# ----- Linkedin ------
+
 # api.update_status("Hello world")
 # if latest_repo :
 #     print("Do you want to post the latest repo on Twitter ?")
