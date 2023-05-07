6b350083db4f04a63b5591219cd86bdcebfa8d3b
Thabish <kadertabish2@gmail.com>
2023-05-07 11:40:28 +0400

    ci-cd
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ CICD_tweet.py
@@ -0,0 +1,41 @@
+import os
+from github import Github
+from dotenv import load_dotenv
+import tweepy
+import re
+
+load_dotenv()
+github_acess_token = os.environ.get('ACESS_TOKEN')
+consumer_key = os.environ.get('TWITTER_API_KEY')
+consumer_secret = os.environ.get('TWITTER_API_SECRET')
+twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+twitter_access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+twitter_bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
+
+# #  ----- Github ------ 
+g = Github(github_acess_token)
+latest_repo = None
+# Loop over repos and find the latest repo
+for repo in g.get_user().get_repos():
+    if not latest_repo or repo.updated_at > latest_repo.updated_at:
+        latest_repo = repo
+contents = latest_repo.get_contents("readme.md")
+readme_content = contents.decoded_content.decode("utf-8")
+formatted_readme = re.cleaned_content = re.sub(r'^#+\s*', '', readme_content, flags=re.MULTILINE)
+
+# # ----- Twitter ------
+
+client = tweepy.Client(
+   bearer_token=twitter_bearer_token,
+   consumer_key=consumer_key,
+   consumer_secret=consumer_secret,
+   access_token=twitter_access_token,
+   access_token_secret=twitter_access_token_secret
+)
+
+# ----- Post on Twitter ------
+tweet_text=f"Check out my latest GitHub repository: {latest_repo.full_name} - {formatted_readme} {latest_repo.html_url}"
+client.create_tweet(text=tweet_text)
+
+
+
