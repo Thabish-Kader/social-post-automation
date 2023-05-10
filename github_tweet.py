bfee28262fb2baf8080d863f15f18c3b6fd6a32a
Thabish <kadertabish2@gmail.com>
2023-05-10 16:00:39 +0400

    change
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- github_tweet.py
+++ github_tweet.py
@@ -14,16 +14,18 @@ twitter_bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
 
 # #  ----- Github ------ 
 g = Github(github_acess_token)
-latest_repo = None
+social_post_automation_repo = None
 # Loop over repos and find the latest repo
 for repo in g.get_user().get_repos():
-    if not latest_repo or repo.updated_at > latest_repo.updated_at:
-        latest_repo = repo
-contents = latest_repo.get_contents("readme.md")
+    if repo.name == "social-post-automation":
+        social_post_automation_repo = repo
+
+contents = social_post_automation_repo.get_contents("readme.md")
 readme_content = contents.decoded_content.decode("utf-8")
 formatted_readme = re.cleaned_content = re.sub(r'^#+\s*', '', readme_content, flags=re.MULTILINE)
 
-# # ----- Twitter ------
+
+# ----- Twitter ------
 
 client = tweepy.Client(
    bearer_token=twitter_bearer_token,
@@ -36,12 +38,12 @@ client = tweepy.Client(
 # ----- Post on Twitter ------
 
 
-if latest_repo :
+if social_post_automation_repo :
     print("Do you want to post the latest repo on Twitter ?")
     answer = input("Y/N : ")
     if answer.lower() == "y" :
         try :
-            tweet_text=f"Check out my latest GitHub repository: {latest_repo.full_name} - {formatted_readme} {latest_repo.html_url}"
+            tweet_text=f"Check out my latest GitHub repository: {social_post_automation_repo.full_name} - {formatted_readme} {social_post_automation_repo.html_url}"
             client.create_tweet(text=tweet_text)
             print("Posted on Twitter!")
         except PermissionError:
